d213575e48aeb4c3083f8425123db18e3b0acb5c
Jakub Wilk <jwilk@jwilk.net>
2006-07-27 12:03:28 +0200

    Release 0.3.
    Completely new configuration file
    
    Drop phonebook support (use external programs instead: “Person2Number”,
    “Number2Person” configuration options)
    
    Drop “--debug” option (use “Debug” configuration option instead).
    
    Actions “-s” and “-S” does not differ any more (use “RejectUnpersons”
    configuration option or “--force” option instead).
    
    Change file names: “orange-pl-cookie-jar.txt” → “cookie-jar.txt”.

--- orange.pl
+++ orange.pl
@@ -4,49 +4,128 @@ use strict;
 use warnings;
 
 use HTTP::Request::Common qw(GET POST);
-use HTTP::Cookies qw();
-use LWP::UserAgent qw();
-use Crypt::SSLeay qw();
+use HTTP::Cookies ();
+use LWP::UserAgent ();
+use Crypt::SSLeay ();
 use Fcntl qw(:flock :DEFAULT);
 use Getopt::Long qw(:config gnu_getopt no_ignore_case);
 use Pod::Usage qw(pod2usage);
+use Apache::ConfigFile ();
 
+our $VERSION = '0.20060727';
+my $site = 'orange.pl';
+my $cookie_domain = '.orange.pl';
+my $software_name = 'orange-pl';
+my $config_file = 'orange-pl.conf';
 my $debug = 0;
+my $person2number;
+my $number2person;
+my $reject_unpersons = 0;
 
 sub quit { printf STDERR "%s\n", shift; exit 1; }
 
-sub api_error 
-{ 
-  my $message = 'API error';
-  if ($debug)
-  {
-    my $code = shift;
-    $message .= " (code: $code)";
-  }
+sub error
+{
+  my ($message, $code) = @_;
+  $message .= " ($code)" if $debug;
   quit $message; 
 }
 
-sub debug { printf STDERR "%s\n", shift if $debug; };
+sub api_error { error 'API error', "code: $_[0]"; }
 
-sub gsm_complete_net
-{
-  $_ = shift;
-  my $net = 'invalid';
-  $net = 'orange.gsm'  if /^5[0-9]{8}$/;
-  $net = 'era.gsm'     if /^6[0-9][02468][0-9]{6}$/;
-  $net = 'plus.gsm'    if /^6[0-9][13579][0-9]{6}$/;
-  return "$_\@$net";
-}
+sub http_error { error 'HTTP error', $_[0] }
+
+sub debug { printf STDERR "%s\n", shift if $debug; };
 
 sub lwp_init
 {
   my $ua = new LWP::UserAgent;
   $ua->timeout(30);
-  $ua->agent('Mozilla/4.7 [en] (WinNT; I)');
+  $ua->agent('Mozilla/5.0');
   $ua->env_proxy();
-  $ua->cookie_jar(HTTP::Cookies->new(file => './orange-pl-cookie-jar.txt', autosave => 1, ignore_discard => 1));
+  $ua->cookie_jar(HTTP::Cookies->new(file => './cookie-jar.txt', autosave => 1, ignore_discard => 1));
   push @{$ua->requests_redirectable}, 'POST';
   return $ua;
+}
+
+sub expand_tilde
+{
+  ($_) = @_;
+  s{^~([^/]*)}{length $1 > 0 ? (getpwnam($1))[7] : ( $ENV{'HOME'} || $ENV{'LOGDIR'} )}e;
+  return $_;
+}
+
+sub transliterate
+{
+  require IPC::Open3; import IPC::Open3 qw(open3);
+  local $/;
+  my ($text) = @_;
+  my $pid = open3(\*TEXT, \*TEXT_ASCII, undef, '/usr/bin/konwert', 'utf8-ascii') or quit q{Can't invoke `konwert'};
+  binmode(TEXT, ':encoding(utf-8)');
+  print TEXT $text;
+  close TEXT;
+  $text = <TEXT_ASCII>;
+  close TEXT_ASCII;
+  waitpid $pid, 0;
+  return $text;
+}
+
+sub codeset
+{
+  require I18N::Langinfo; import I18N::Langinfo qw(langinfo CODESET);
+  my $codeset = langinfo(CODESET()) or die;
+  return $codeset;
+}
+
+sub resolve_number
+{
+  my ($number) = @_;
+  if (defined $number2person)
+  {
+    open N2P, '-|:encoding(utf-8)', $number2person, $number or quit q(Can't invoke resolver);
+    $_ = <N2P>;
+    close N2P;
+    my ($person) = split /\t/ if defined $_;
+    return "$person <$number>" if defined $person;
+  }
+  return undef if $reject_unpersons;
+  return "<$number>";
+}
+
+sub resolve_recipient
+{
+  my ($number, $recipient);
+  ($recipient) = @_;
+  if ($recipient =~ /[^+0-9]/ and defined $person2number)
+  {
+    open P2N, '-|:encoding(utf-8)', $person2number, $recipient or quit q(Can't invoke resolver);
+    my @phonebook = <P2N>;
+    close P2N;
+    if ($#phonebook == 0)
+    {
+      ($_, $number) = split /\t/, $phonebook[0];
+    }
+    elsif ($#phonebook > 0)
+    {
+      print STDERR "Ambiguous recipient, please make up your mind:\n";
+      print STDERR "  $_" foreach @phonebook;
+      exit 1;
+    }
+    else
+    {
+      $number = '';
+    }
+  }
+  else
+  {
+    $number = $recipient;
+  }
+  quit 'No such recipient' unless $number =~ /^(\+48)?([0-9]{9})$|^([0-9]{11})$/;
+  $number = $3 if defined $3;
+  $number = $2 if defined $2;
+  $recipient = resolve_number($number);
+  quit 'No such recipient' unless defined $recipient;
+  return ($number, $recipient);
 }
 
 sub lwp_visit
@@ -54,136 +133,147 @@ sub lwp_visit
   my $ua = shift;
   my $uri = shift;
   my $res = $ua->request(GET $uri);
-  quit "Can't open $uri" unless $res->is_success;
+  http_error $uri unless $res->is_success;
   return $res;
 }
 
-our $VERSION = '0.20060714';
+my $list_limit;
+my $list_expand;
 my $action = 's';
+my $force = 0;
 GetOptions(
-  'send|s' =>       sub { $action = 's'; },
-  'force-send|S' => sub { $action = 'S'; },
-  'count|c' =>      sub { $action = 'c'; },
-  'info|i' =>       sub { $action = 'i'; },
-  'list-sent|l' =>  sub { $action = 'l'; },
-  'list-inbox|m' => sub { $action = 'm'; },
-  'version' =>      sub { quit "orange.pl $VERSION"; },
-  'debug' =>        \$debug,
-  'help|h|?' =>     sub { pod2usage(1); }
+  'send|s|S' =>       sub { $action = 's'; },
+  'count|c' =>        sub { $action = 'c'; },
+  'logout' =>         sub { $action = '0'; },
+  'info|i' =>         sub { $action = 'i'; },
+  'list-sent|l:i'  => sub { $action = 'l'; ($_, $list_limit) = @_; },
+  'list-inbox|m:i' => sub { $action = 'm'; ($_, $list_limit) = @_; },
+  'expand' =>         \$list_expand,
+  'force' =>          \$force,
+  'version' =>        sub { quit "$software_name, version $VERSION"; },
+  'debug' =>          \$debug,
+  'help|h|?' =>       sub { pod2usage(1); }
 ) or pod2usage(1);
+if (defined $list_limit)
+{
+  pod2usage(1) unless $list_limit =~ /^[0-9]{1,4}$/;
+  $list_limit = 9999 if $list_limit == 0;
+}
 
-my $orange_home = exists $ENV{'ORANGEPL_HOME'} ? $ENV{'ORANGEPL_HOME'} : "$ENV{'HOME'}/.orange-pl/";
-chdir $orange_home or quit "Can't change working directory to $orange_home";
+my $env = $software_name;
+$env =~ s/\W//g;
+$env =~ y/a-z/A-Z/;
+$env .= '_HOME';
+my $home = exists $ENV{$env} ? $ENV{$env} : "$ENV{'HOME'}/.$software_name/";
+chdir $home or quit "Can't change working directory to $home";
+
+my $ua = lwp_init();
+
+my $login = '';
+my $password;
+
+my %conf_vars = 
+(
+  'login' => sub 
+    { $login = shift; },
+  'password' => sub 
+    { $password = shift; },
+  'password64' => sub
+    { 
+      use MIME::Base64 ();
+      $password = MIME::Base64::decode(shift);
+    },
+  'number2person' => sub 
+    { $number2person = expand_tilde(shift); },
+  'person2number' => sub 
+    { $person2number = expand_tilde(shift); },
+  'reject_unpersons' => sub 
+    { $reject_unpersons = parse_yes_no(shift); },
+  'debug' => sub 
+    { $debug = shift; },
+);
+
+my $ac = Apache::ConfigFile->read(file => $config_file, ignore_case => 1, fix_booleans => 1, raise_error => 1);
+foreach my $context (($ac, $ac->cmd_context(site => $site)))
+{
+  foreach my $var (keys %conf_vars)
+  {
+    my $val = $context->cmd_config($var);
+    $conf_vars{$var}($val) if defined $val;
+  }
+}
+
+$reject_unpersons = 0 if $force;
+quit 'No login name provided' unless length $login > 0;
+quit 'No password provided' unless defined $password;
 
 
-my $ua = lwp_init;
-
-open CONF, '<', './orange-pl.conf' or quit q(Can't open the configuration file);
-flock CONF, LOCK_SH or quit q(Can't lock the configuration file);
-my $username = <CONF>; chomp $username;
-my $password = <CONF>; chomp $password;
-close CONF;
-
-debug "Username: $username\@orange.pl";
+debug "Login: $login\@$site";
 
 my $number;
-my $message;
-my $message_len;
+my $body;
+my $body_len;
 
-if ($action eq 'S' || $action eq 's')
+
+if ($action eq 's')
 {
   pod2usage(1) if $#ARGV != 1;
 
-  require I18N::Langinfo; import I18N::Langinfo qw(langinfo CODESET);
-  require Encode; import Encode qw(decode encode);
-  require IPC::Open3; import IPC::Open3 qw(open3);
+  require Encode; import Encode qw(encode decode);
   require Text::Wrap; import Text::Wrap qw(wrap);
-  my $codeset = langinfo(CODESET()) or die;
+  my $codeset = codeset();
   debug "Codeset: $codeset";
-
-  $number = decode($codeset, $ARGV[0]);
-  $message = decode($codeset, $ARGV[1]);
-   
-  $number =~ s/^\+48//;
-  $number =~ s/^00//;
-  my $grep = $number !~ /^[0-9]{9,11}$/;
-  my ($fnumber, $fname);
-  open PHONEBOOK, '<:encoding(UTF-8)', $ENV{'PHONEBOOK'} or quit q(Can't open the phonebook);
-  flock PHONEBOOK, LOCK_SH or quit q(Can't lock the phonebook);
-  my $found = 0;
-  while (<PHONEBOOK>)
-  {
-    next unless /^[^#]/;
-    my ($cname, $place, $cnumber, $tmp) = split /\t/;
-    next unless defined($place);
-    next unless $place eq '*';
-    if (($grep && index($cname, $number) >= 0) || (!$grep && $cnumber == $number))
-    {
-      quit 'Invalid argument: ambiguous recipient' if $found++;
-      ($fnumber, $fname) = ($cnumber, $cname);
-    }
-  }
-  close PHONEBOOK;
-  $found = !$grep if $action eq 'S';
-  quit 'Invalid argument: no such recipient' unless $found;
-  $fnumber = $number unless defined $fnumber;
-  chomp $fnumber;
-  $number = $fnumber;
-  $fnumber = gsm_complete_net $fnumber;
-  $fname = (defined $fname) ? (' ' . encode($codeset, $fname)) : '';
-  debug "Recipient:$fname <$fnumber>";
-
-  my $pid = open3(\*MESSAGE, \*MESSAGE_ASCII, undef, '/usr/bin/konwert', 'utf8-ascii') or quit q{Can't invoke `konwert'};
-  $message = encode('UTF-8', $message);
-  print MESSAGE $message;
-  close MESSAGE;
-  {
-    local $/;
-    $message = <MESSAGE_ASCII>;
-  }
-  close MESSAGE_ASCII;
-  waitpid $pid, 0;
-  debug "Message: \n" . wrap("  ", "  ", $message);
-  $message_len = length $message;
-  debug "Message length: $message_len";
-  quit "Message too long ($message_len > 640)" if $message_len > 640;
-  $action = 'S';
+  binmode STDERR, ":encoding($codeset)";
+  binmode STDOUT, ":encoding($codeset)";
+  
+  (my $recipient, $body) = @ARGV;
+  $recipient = decode($codeset, $recipient);
+  $body = decode($codeset, $body);
+  ($number, $recipient) = resolve_recipient $recipient;
+  debug "Recipient: $recipient";
+  $body = transliterate($body); 
+  debug "Message: \n" . wrap("  ", "  ", $body);
+  $body_len = length $body;
+  debug "Message length: $body_len";
+  quit "Message too long ($body_len > 640)" if $body_len > 640;
 }
-elsif ($action !~ '^[cilm]$')
+elsif ($action eq '0')
 {
-  quit "Unknow action: $action";
+  $ua->cookie_jar->clear($cookie_domain);
+  debug 'Cookies has been purged';
+  exit;
 }
 
-my $req;
 my $res;
 
-$res = lwp_visit $ua, 'http://www.orange.pl/portal/map/map/signin';
+my $signin_uri = 'http://www.orange.pl/portal/map/map/signin';
+$res = lwp_visit $ua, $signin_uri;
 unless ($res->content =~ /zalogowany jako/i)
 {
   debug 'Logging in...';
   my $uri = 'https://www.orange.pl/portal/map/map/signin?_DARGS=/gear/static/signIn.jsp';
   my $a = '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler';
-  $req = POST $uri, 
+  my $req = POST $uri, 
     [
       "$a.login" => ' ', "$a.login.x" => 0, "$a.login.y" => 0,
       "$a.loginErrorURL" => 'http://www.orange.pl/portal/map/map/signin',
       "$a.loginSuccessURL" => 'http://www.orange.pl/portal/map/map/pim',
-      "$a.value.login" => $username, "$a.value.password" => $password,
+      "$a.value.login" => $login, "$a.value.password" => $password,
       "_D:$a.login" => ' ', "_D:$a.loginErrorURL" => ' ', "_D:$a.loginSuccessURL" => ' ',
       "_D:$a.value.login" => ' ', "_D:$a.value.password" => ' ',
       '_DARGS' => '/gear/static/signIn.jsp',
       '_dyncharset' => 'UTF-8'
     ];
-  $req->referer('http://www.orange.pl/portal/map/map/signin');
+  $req->referer($signin_uri);
   $res = $ua->request($req);
-  quit "Login error ($uri)" unless $res->is_success;
+  http_error $signin_uri unless $res->is_success();
   quit 'Login error: incorrect password, I guess...' unless $res->content =~ /zalogowany jako/i;
 }
 debug 'Logged in!';
 
 sub extract_remaining
 {
-  $_ = shift;
+  ($_) = @_;
   if (m{<div id="syndication">(.*?)</div>}s)
   {
     $_ = $1;
@@ -199,11 +289,11 @@ my $remaining = extract_remaining $res->content;
 if ($action eq 'c')
 {
   print "Number of remaining messages: $remaining\n";
-  exit 0;
 }
 elsif ($action eq 'i')
 {
-  $res = lwp_visit $ua, 'http://online.orange.pl/portal/ecare';
+  my $uri = 'http://online.orange.pl/portal/ecare';
+  $res = lwp_visit $ua, $uri;
   $_ = $res->content;
   s/\s+/ /g;
   api_error 'i1' unless m{<div id="tbl-list3">.*?<table>(.*?)</table>};
@@ -238,55 +328,36 @@ elsif ($action eq 'i')
 }
 elsif ($action eq 'l' || $action eq 'm')
 {
-  require I18N::Langinfo; import I18N::Langinfo qw(langinfo CODESET);
-  require Encode; import Encode qw(encode from_to);
+  require Encode; import Encode qw(decode);
   require HTML::Entities; import HTML::Entities qw(decode_entities);
 
-  my $codeset = langinfo(CODESET()) or die;
+  my $codeset = codeset();
   debug "Codeset: $codeset";
+  binmode STDOUT, ":encoding($codeset)";
 
   my $pg;
   $pg = 'sentmessageslist' if $action eq 'l';
   $pg = 'messageslist' if $action eq 'm';
-  $res = lwp_visit $ua, 'http://www.orange.pl/portal/map/map/message_box?mbox_view=' . $pg;
+  $res = lwp_visit $ua, "http://www.orange.pl/portal/map/map/message_box?mbox_view=$pg";
   $_ = $res->content;
   s/\s+/ /g;
   api_error 'l1' unless m{<table id="list">(.*?)</table>};
   $_ = $1;
   s{<thead>.*?</thead>}{};
   s{<tfoot>.*?</tfoot>}{};
+  my @urls = m{(?<=<a href=")/portal/map/map/message_box\?[^"]*(?=">)}g;
   s{</?a.*?>}{}g;
   my @list = m{<td.*?>(.*?)</td>}g;
-
-  my %phonebook;
-  if (open PHONEBOOK, '<:encoding(UTF-8)', $ENV{'PHONEBOOK'})
-  {
-    flock PHONEBOOK, LOCK_SH or quit q{Can't lock the phonebook};
-    while (<PHONEBOOK>)
-    {
-      next unless /^[^#]/;
-      my ($cname, $place, $cnumber, $tmp) = split /\t/;
-      next unless defined($place);
-      next unless $place eq '*' or $place eq '<mbox>';
-      $phonebook{$cnumber} = $cname;
-    }
-    close PHONEBOOK;
-  }
-  else 
-  {
-    debug q{Can't open the phonebook};
-  }
-  
-  while ($#list >= 4)
+ 
+  while ($#list >= 4 && $list_limit > 0)
   {
     shift @list; shift @list;
-    my $cnumber = shift @list;
-    my $cnumber2 = gsm_complete_net $cnumber;
-    my $cname = $cnumber;
-    $cname = encode($codeset, "$phonebook{$cnumber} <$cnumber2>", 'UTF-8') if exists $phonebook{$cnumber};
+    my $url = shift @urls;
+    api_error 'l3' if $url ne shift @urls;
+    my $cname = resolve_number shift @list;
     my $text = shift @list;
     decode_entities($text);
-    from_to($text, 'UTF-8', $codeset);
+    $text = decode('UTF-8', $text);
     my $date = shift @list;
     $date =~ s/ /, /;
     my $hdr = 'To';
@@ -300,20 +371,32 @@ elsif ($action eq 'l' || $action eq 'm')
       $status = 'delivered' if $status =~ /^dos/;
       print "Status: $status\n";
     }
+    if ($list_expand)
+    {
+      require Text::Wrap; import Text::Wrap qw(wrap);
+      $res = lwp_visit $ua, "http://www.orange.pl$url";
+      $_ = $res->content;
+      api_error 'l4' unless m{<div class="message-body"><pre>(.*)</pre></div>}s;
+      $text = decode_entities($1);
+      $text = decode('UTF-8', $text);
+      $text = "\n" . wrap("  ", "  ", $text);
+    }
     print "Contents: $text\n\n";
+    $list_limit--;
   }
 }
-elsif ($action eq 'S')
+elsif ($action eq 's')
 {
   quit 'Message limit exceeded' if $remaining == 0;
-  lwp_visit $ua, 'http://www.orange.pl/portal/map/map/message_box?mbox_view=newsms&mbox_edit=new';
+  my $newmsg_uri = 'http://www.orange.pl/portal/map/map/message_box?mbox_view=newsms&mbox_edit=new';
+  $res = lwp_visit $ua, $newmsg_uri;
   debug 'Ready to send...';
   my $uri = 'http://www.orange.pl/portal/map/map/message_box??_DARGS=/gear/mapmessagebox/smsform.jsp';
   my $a = '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler';
-  $req = POST $uri, 
+  my $req = POST $uri, 
     [
       '_dyncharset' => 'UTF-8',
-      "$a.body" => $message,
+      "$a.body" => $body,
       "$a.create.x" => 0, "$a.create.y" => 0,
       "$a.errorURL" => '/portal/map/map/message_box?mbox_view=newsms',
       "$a.successURL" => '/portal/map/map/message_box?mbox_view=messageslist',
@@ -321,16 +404,16 @@ elsif ($action eq 'S')
       "_D:$a.body" => ' ', "_D:$a.create" => ' ', "_D:$a.errorURL"  => ' ', 
       "_D:$a.successURL" => ' ', "_D:$a.to" => ' ', "_D:$a.type" => ' ',
       '_DARGS' => '/gear/mapmessagebox/smsform.jsp',
-      'counter' => 640 - $message_len
+      'counter' => 640 - $body_len
     ];
-  $req->referer('http://www.orange.pl/portal/map/map/message_box?mbox_view=newsms&mbox_edit=new');
+  $req->referer($newmsg_uri);
   debug 'Sending...';
   $res = $ua->request($req);
-  quit "Error while sending the message ($uri)" unless $res->is_success;
+  http_error $uri unless $res->is_success;
   my $remaining_after = extract_remaining $res->content;
   quit 'Error while sending the message, I guess...' unless $remaining_after < $remaining;
   debug 'Looks OK';
-  debug "Number of remaining messages: $remaining_after\n";
+  debug "Number of remaining messages: $remaining_after";
 }
 
 __END__
@@ -343,17 +426,15 @@ orange.pl -- send SMs via orange.pl gateway
 
 =over 4
 
-=item orange [-s] I<< <phonebook-entry> >> I<< <text> >>
+=item orange.pl [-s] [--force] I<< <phonebook-entry> >> I<< <text> >>
 
-=item orange -S I<< <phone-number> >> I<< <text> >>
+=item orange.pl -c
 
-=item orange -c
+=item orange.pl -l [I<N>] [--expand]
 
-=item orange -l
+=item orange.pl -m [I<N>] [--expand]
 
-=item orange -m
-
-=item orange -i
+=item orange.pl -i
 
 =back
 
@@ -367,21 +448,7 @@ ORANGEPL_HOME (default: F<$HOME/.orange-pl/>)
 
 =item F<$ORANGEPL_HOME/orange-pl.conf>
 
-The configuration file is in the following format:
-
-I<[login-name]>  I<[password]>
-
-where fields are newline-separated.
-
-=item F<$ORANGEPL_HOME/orange-pl-cookie-jar.txt>
-
-=item F<$ORANGEPL_HOME/phonebook>
-
-The phonebook is consisted of lines in the following format:
-
-I<[personal-name]>  I<[place]>  I<[phone-number]>
-
-where fields are tab-separated; I<[place]> must be set to C<*> for a cellular phone number, or to C<< <mbox> >> for a mbox phone number.
+=item F<$ORANGEPL_HOME/cookie-jar.txt>
 
 =back
 
